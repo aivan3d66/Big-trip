aaaaad4a86ee1228a68ce3ea2515618fd5dba0c1
Ivan <adamovskij00@mail.ru>
2022-03-21 11:44:35 +0300

    add upd bundle.js

--- bundle.js
+++ bundle.js
@@ -45001,7 +45001,7 @@ const createEventTypeMenuButton = (id, type) => {
   return `<div class="event__type-wrapper">
             <label class="event__type  event__type-btn" for="event-type-toggle-${id}">
               <span class="visually-hidden">Choose event type</span>
-              <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
+              <img class="event__type-icon" width="17" height="17" src="./public/img/icons/${type}.png" alt="Event type icon">
             </label>
             <input class="event__type-toggle  visually-hidden" id="event-type-toggle-${id}" type="checkbox">
             <div class="event__type-list">
@@ -45332,7 +45332,7 @@ const createDate = (from) => {
 };
 
 const createType = (type) => {
-  return `<div class="event__type"><img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon"></div>`;
+  return `<div class="event__type"><img class="event__type-icon" width="42" height="42" src="./public/img/icons/${type}.png" alt="Event type icon"></div>`;
 };
 
 const createDestinationTitle = (name) => {
